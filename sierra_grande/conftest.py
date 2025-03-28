a0a3dcafd68ccc18984af8940155e80f35475824
Alberto Mimbrero <devs.alb@pm.me>
2025-03-28 10:58:49 +0100

    corrections template

---
+++ sierra_grande/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sierra_grande.users.models import User
+from sierra_grande.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
